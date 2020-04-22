0376d6e1a314d9af2a7cf7260b128de83a9d82d1
marco_dca <marcodecara@gmail.com>
2020-04-22 10:54:13 +0200

    Getting started with basic setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const template = React.createElement("h2", null, "Just testing");
+
+ReactDOM.render(template, document.querySelector("#root"));
