38a0d037796c1675c56cda6fd4ba6b09e102f216
marco_dca <marcodecara@gmail.com>
2020-04-22 12:10:21 +0200

    adding styled-components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 
-const App = () => <h1>Testing</h1>;
+const H1 = styled.h1`
+  color: red;
+`;
+
+const App = () => <H1>Testing it</H1>;
 
 ReactDOM.render(<App />, document.querySelector("#root"));
