82d3983cfed0b08e0959e90f6277da3e4d0cda04
marco_dca <marcodecara@gmail.com>
2020-04-22 15:04:25 +0200

    installing and setting dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,30 @@
 const path = require("path");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
 
-module.exports = {
-  entry: ["@babel/polyfill", "./src/index.js"],
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-  },
+module.exports = () => {
+  const env = dotenv.config().parsed;
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: ["@babel/polyfill", "./src/index.js"],
+    output: {
+      path: path.resolve(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      ],
+    },
+    devtool: "cheap-module-eval-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+    },
+    plugins: [new webpack.DefinePlugin(envKeys)],
+  };
 };
