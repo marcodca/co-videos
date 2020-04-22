2c652d0cff314cd024cf77c6f9e8e5275d0e0492
marco_dca <marcodecara@gmail.com>
2020-04-22 12:47:38 +0200

    Setting testing-library

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  color: red;
+`;
+
+const App = () => <H1>Testing it</H1>;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import styled from "styled-components";
-
-const H1 = styled.h1`
-  color: red;
-`;
-
-const App = () => <H1>Testing it</H1>;
+import App from "./App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
