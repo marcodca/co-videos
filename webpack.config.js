7af9d83943519c81c55406890d6f0deb4dec36f0
marco_dca <marcodecara@gmail.com>
2020-04-23 18:25:07 +0200

    fixing webpack config for routing

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,46 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = () => {
   return {
-    entry: ["@babel/polyfill", "./src/index.js"],
+    entry: ["@babel/polyfill", "./index.js"],
     output: {
       path: path.resolve(__dirname, "public"),
+      publicPath: '/',
       filename: "bundle.js",
     },
+    context: path.resolve(__dirname, "src"),
     module: {
       rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+        { test: /\.js?$/, exclude: /node_modules/, loader: "babel-loader" },
+        {
+          test: /\.(jpg|png|gif|svg)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                outputPath: "./assets/",
+              },
+            },
+          ],
+        },
       ],
     },
     devtool: "cheap-module-eval-source-map",
     devServer: {
-      contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
+      stats: "errors-only",
+      open: true,
+      port: 8080,
+      compress: true,
     },
-    plugins: [new Dotenv()],
+    plugins: [
+      new Dotenv(),
+      new HtmlWebpackPlugin({
+        template: "index.html",
+      }),
+    ],
   };
 };
