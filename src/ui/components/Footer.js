65a8f5d66d9b61b647f7e558aaaf49193802c6a2
marco_dca <marcodecara@gmail.com>
2020-04-25 18:18:48 +0200

    setting footer

--- src/ui/components/Footer.js
+++ src/ui/components/Footer.js
@@ -1,5 +1,46 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../../assets/logo.png";
 
 export const Footer = () => {
-  return <footer>In the bottom the footer stands</footer>;
+  return (
+    <Container>
+      <span>
+        Powered by{" "}
+        <a href="https://www.themoviedb.org/" title="The movie DB">
+          TheMovieDB
+        </a>
+      </span>
+
+      <span>
+        Icons by {""}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </span>
+      <span>
+        By
+        <a href="https://www.marcodecara.com/" title="Marco de Cara">
+          <img width={30} src={logo} alt="marco de cara's logo" />
+        </a>
+      </span>
+    </Container>
+  );
 };
+
+const Container = styled.footer`
+  background: var(--color-gray-light);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 2.7em;
+  font-size: 0.8em;
+  text-align: center;
+  span:last-child {
+    display: flex;
+    align-items: center;
+    img {
+      margin-left: 5px;
+    }
+  }
+`;
