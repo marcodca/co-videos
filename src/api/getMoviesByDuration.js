69f640c9434b4cf4f1c9ffef00ec39e74cab1788
marco_dca <marcodecara@gmail.com>
2020-04-26 21:19:26 +0200

    api twik

--- src/api/getMoviesByDuration.js
+++ src/api/getMoviesByDuration.js
@@ -4,6 +4,6 @@ import sendQuery from "./helpers/sendQuery";
 
 export default ({ payload, page, sortBy = "popularity.desc" }) =>
   sendQuery(
-    `sort_by=${sortBy}&page=${page}&with_runtime.lte=${payload.max}`,
+    `sort_by=${sortBy}&page=${page}&with_runtime.gte=${payload.min}&with_runtime.lte=${payload.max}`,
     "discover"
   );
