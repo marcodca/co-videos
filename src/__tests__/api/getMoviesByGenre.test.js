61bc96cc1433b9f5e0b7f11449bac1a8ee5ad4e5
marco_dca <marcodecara@gmail.com>
2020-04-27 21:06:48 +0200

    api testing

---
+++ src/__tests__/api/getMoviesByGenre.test.js
@@ -0,0 +1,36 @@
+import { getMoviesByGenre } from "../../api";
+import sendQuery from "../../api/helpers/sendQuery";
+
+jest.mock("../../api/helpers/sendQuery");
+
+beforeEach(jest.clearAllMocks);
+
+//Giving the similarity between all the api functions, we are only testing this one for the time being.
+
+describe("getMoviesByGenre", () => {
+  it("sends correctly the params to sendQuery", () => {
+    sendQuery.mockImplementation((a) => a);
+    const resp = getMoviesByGenre({
+      payload: { id: 1 },
+      page: 5,
+      sortBy: "something",
+    });
+
+    expect.assertions(3);
+    expect(resp).toBe("sort_by=something&page=5&with_genres=1");
+    expect(sendQuery).toHaveBeenCalledTimes(1);
+    expect(sendQuery).toHaveBeenCalledWith(resp, "discover");
+  });
+  it("use the default parameter for sortBy", () => {
+    sendQuery.mockImplementation((a) => a);
+    const resp = getMoviesByGenre({
+      payload: { id: 3 },
+      page: 19,
+    });
+
+    expect.assertions(3);
+    expect(resp).toBe("sort_by=popularity.desc&page=19&with_genres=3");
+    expect(sendQuery).toHaveBeenCalledTimes(1);
+    expect(sendQuery).toHaveBeenCalledWith(resp, "discover");
+  });
+});
