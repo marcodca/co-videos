c23d09e66a400566ab3516ba30e52a28f6d83c9b
marco_dca <marcodecara@gmail.com>
2020-04-27 14:21:34 +0200

    clearing css

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,5 +1,4 @@
 import { createGlobalStyle } from "styled-components";
-import media from "./mediaUtil";
 
 export default createGlobalStyle`
     html,
@@ -12,15 +11,18 @@ export default createGlobalStyle`
     }
 
     :root{
-        --color-primary: #00bcd4;
         --color-secondary: #ff5722;
         --color-gray: 	#808080;
         --color-gray-light: #D3D3D3;
         --color-gray-dark: #2F4F4F;
     }
     body {
-        ${media.md`font-size: 1.2em`};
-        ${media.lg`font-size: 1.4em`};
+        @media screen and (min-width: 768px) {
+            font-size: 1.2em
+        }
+        @media screen and (min-width: 1240px) {
+            font-size: 1.4em
+        }
         transition: font-size 0.3s;
         font-family: 'Merriweather', serif;
         color: var(--color-gray-dark);
